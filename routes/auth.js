199c33aafba61a450970a32493dba1fdb5a9b599
nikhilmehta16 <57535063+nikhilmehta16@users.noreply.github.com>
2020-08-28 18:45:44 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authrouter = express.Router();
 const User = require("../models/RegisterSchema");
+
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-authrouter.route("/").post(async (req, res) => {
+const Admin = require("../models/AdminSchema");
+authrouter.route("/user/auth").post(async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   console.log(user);
   if (!user) {
@@ -22,6 +24,24 @@ authrouter.route("/").post(async (req, res) => {
         const token = user.generateAuthToken();
         res.send(token);
       }
+    }
+  }
+});
+authrouter.route("/admin/auth").post(async (req, res) => {
+  let user = await Admin.findOne({ email: req.body.email });
+  console.log(user);
+  if (!user) {
+    return res.status(400).send("Invalid E-mail or Password.");
+  } else {
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(400).send("Invalid E-mail or Password.");
+    } else {
+      const token = user.generateAuthToken();
+      res.send(token);
     }
   }
 });
